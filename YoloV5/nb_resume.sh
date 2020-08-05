e418a0e074a92f897a3ab2d2af0fb358f5af82b3
ZhangZisu <i@zzs1.cn>
2020-08-05 20:56:10 +0800

    Update

--- YoloV5/nb_resume.sh
+++ YoloV5/nb_resume.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 pip install -r requirements.txt
+rm ../Temp/data_yolo/images/*.cache
 python train.py --img 640 --batch 16 --epochs 50 --data ./train/dataset.yml --cfg ./train/yolov5x.yml --resume
