cfa6f4f28916fb20817ed09bf72cc51923fad24c
ZhangZisu <i@zzs1.cn>
2020-08-05 20:19:08 +0800

    Add yolo

---
+++ DataConverter/yolo.js
@@ -0,0 +1,87 @@
+const xml = require('fast-xml-parser')
+const fs = require('fs-extra')
+const path = require('path')
+const { SingleBar } = require('cli-progress')
+
+const argv = require('yargs').argv
+
+const W = 1280
+const H = 720
+
+const root = path.resolve(__dirname, '..')
+const temp = path.join(root, 'Temp')
+// @ts-ignore
+const input = path.resolve(argv.input)
+const output = path.join(temp, `data_coco_out`)
+const bar = new SingleBar({})
+const classes = ['red_stop', 'green_go', 'yellow_back', 'pedestrian_crossing', 'speed_limited', 'speed_unlimited']
+const index = Object.create(null)
+classes.forEach((x, i) => index[x] = i)
+
+fs.ensureDirSync(output)
+
+/**
+ * @param {string} alias 
+ * @param {string[]} names
+ */
+function generate(alias, names) {
+  const img_out = path.join(output, 'images/' + alias)
+  const lab_out = path.join(output, 'labels/' + alias)
+  fs.ensureDirSync(img_out)
+  fs.ensureDirSync(lab_out)
+  bar.start(names.length, 0)
+  for (const name of names) {
+    const xml_name = name + '.xml'
+    const img_name = name + '.jpg'
+    const o = xml.parse(fs.readFileSync(path.join(input, xml_name)).toString())
+    const img_path = path.join(img_out, img_name)
+    const lab_path = path.join(lab_out, name + '.txt')
+    fs.copyFileSync(path.join(input, img_name), img_path)
+    if ('object' in o.annotation) {
+      const objects = o.annotation.object instanceof Array ? o.annotation.object : [o.annotation.object]
+      const result = []
+      for (const object of objects) {
+        const x1 = object.bndbox.xmin / W
+        const y1 = object.bndbox.ymin / H
+        const x2 = object.bndbox.xmax / W
+        const y2 = object.bndbox.ymax / H
+        result.push([
+          index[object.name],
+          (x1 + x2) / 2,
+          (y1 + y2) / 2,
+          (x2 - x1),
+          (y2 - y1)
+        ].join(' '))
+      }
+      fs.writeFileSync(lab_path, result.join('\n'))
+    }
+    bar.increment(1)
+  }
+
+  bar.stop()
+}
+
+try {
+  const files = fs.readdirSync(input)
+  const set1 = new Set(files)
+  const set2 = new Set()
+
+  files.forEach(file => set2.add(path.basename(file, path.extname(file))))
+
+  const names = [...set2].filter(x => set1.has(x + '.jpg') && set1.has(x + '.xml'))
+  console.log('Total images', names.length)
+
+  const val = 'val' in argv ? parseFloat(argv.val) : 1
+  const val_count = Math.floor(val * names.length)
+  const data_count = names.length - val_count
+  console.log(`Data / Val = ${data_count} / ${val_count}`)
+  const data = names.splice(0, data_count)
+
+  generate('train', data)
+  generate('val', names)
+} catch (e) {
+  console.error(e)
+  fs.removeSync(output)
+} finally {
+  console.log('DONE')
+}
